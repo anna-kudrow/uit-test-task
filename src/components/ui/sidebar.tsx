e6c4114808f678919cab12c074be52804e4b1dc5
anna <anka-kudrow@mail.ru>
2025-08-18 14:42:35 +0300

    update import

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,5 +1,5 @@
 import { Slot } from "@radix-ui/react-slot";
-import { cva } from "class-variance-authority";
+import { cva, type VariantProps } from "class-variance-authority";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import * as React from "react";
 import { Button } from "@/components/ui/button";
